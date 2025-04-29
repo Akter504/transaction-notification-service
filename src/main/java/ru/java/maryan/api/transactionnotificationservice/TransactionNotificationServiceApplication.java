187e213a8652e23df30a73ca0972b87ff3d542a4
Maryan <maryan.sasha@bk.ru>
2025-04-29 17:58:28 +0300

    Initial commit

---
+++ src/main/java/ru/java/maryan/api/transactionnotificationservice/TransactionNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.java.maryan.api.transactionnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/java/maryan/api/transactionnotificationservice/TransactionNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.maryan.api.transactionnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
