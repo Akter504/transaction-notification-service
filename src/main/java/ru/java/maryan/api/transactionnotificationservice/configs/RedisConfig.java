20198547ec6021eff91adaf469ad4bd86c80e90f
Maryan <maryan.sasha@bk.ru>
2025-05-12 17:12:28 +0300

    Redis cache User

--- src/main/java/ru/java/maryan/api/transactionnotificationservice/configs/RedisConfig.java
+++ src/main/java/ru/java/maryan/api/transactionnotificationservice/configs/RedisConfig.java
@@ -1,11 +1,16 @@
 package ru.java.maryan.api.transactionnotificationservice.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import ru.java.maryan.api.transactionnotificationservice.models.User;
 
 @Configuration
 public class RedisConfig {
@@ -16,6 +21,23 @@ public class RedisConfig {
         template.setConnectionFactory(factory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, User> userRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, User> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<User> serializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, User.class);
+
+        template.setValueSerializer(serializer);
         return template;
     }
 }
--- src/main/java/ru/java/maryan/api/transactionnotificationservice/models/User.java
+++ src/main/java/ru/java/maryan/api/transactionnotificationservice/models/User.java
@@ -1,12 +1,15 @@
 package ru.java.maryan.api.transactionnotificationservice.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class User {
     private Long id;
     private String email;
@@ -14,5 +17,6 @@ public class User {
     private String nameUser;
     private String surnameUser;
     private String passwordHash;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
     private LocalDateTime createdAt;
 }
